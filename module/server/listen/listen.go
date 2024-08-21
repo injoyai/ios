ed4a14ac082ddfad35346abc5f3a57004a3636f3
钱纯净 <1113655791@qq.com>
2024-08-21 22:47:28 +0800

    增加快捷websocket服务

--- module/server/listen/listen.go
+++ module/server/listen/listen.go
@@ -1,10 +1,32 @@
 package listen
 
 import (
+	"github.com/injoyai/ios/module/memory"
 	"github.com/injoyai/ios/module/server"
 	"github.com/injoyai/ios/module/tcp"
+	"github.com/injoyai/ios/module/websocket"
 )
 
-func RunTCP(port int, op ...server.Option) error {
+func TCP(port int, op ...server.Option) (*server.Server, error) {
+	return server.New(tcp.NewListen(port), op...)
+}
+
+func TCPRun(port int, op ...server.Option) error {
 	return server.Run(tcp.NewListen(port), op...)
+}
+
+func Memory(key string, op ...server.Option) (*server.Server, error) {
+	return server.New(memory.NewListen(key), op...)
+}
+
+func MemoryRun(key string, op ...server.Option) error {
+	return server.Run(memory.NewListen(key), op...)
+}
+
+func Websocket(port int, op ...server.Option) (*server.Server, error) {
+	return server.New(websocket.NewListen(port), op...)
+}
+
+func WebsocketRun(port int, op ...server.Option) error {
+	return server.Run(websocket.NewListen(port), op...)
 }
