c5c87fd2e53b49fced81a4eb0f2338b0e501db20
钱纯净 <1113655791@qq.com>
2024-08-21 21:16:40 +0800

    增加快捷访问函数

--- module/client/dial/dial.go
+++ module/client/dial/dial.go
@@ -26,31 +26,53 @@ func TCP(addr string, op ...client.Option) (*client.Client, error) {
 }
 
 func RedialTCP(addr string, op ...client.Option) *client.Client {
-	return client.Redial(tcp.NewDial(addr), func(c *client.Client) {
-		c.SetOption(op...)
-	})
+	return client.Redial(tcp.NewDial(addr), op...)
 }
 
 func SSH(cfg *ssh.Config, op ...client.Option) (*client.Client, error) {
 	return client.Dial(ssh.NewDial(cfg), op...)
 }
 
+func RedialSSH(cfg *ssh.Config, op ...client.Option) *client.Client {
+	return client.Redial(ssh.NewDial(cfg), op...)
+}
+
 func Websocket(addr string, op ...client.Option) (*client.Client, error) {
 	return client.Dial(websocket.NewDial(addr), op...)
+}
+
+func RedialWebsocket(addr string, op ...client.Option) *client.Client {
+	return client.Redial(websocket.NewDial(addr), op...)
 }
 
 func Serial(cfg *serial.Config, op ...client.Option) (*client.Client, error) {
 	return client.Dial(serial.NewDial(cfg), op...)
 }
 
+func RedialSerial(cfg *serial.Config, op ...client.Option) *client.Client {
+	return client.Redial(serial.NewDial(cfg), op...)
+}
+
 func MQTT(cfg *mqtt.Config, subscribe mqtt.Subscribe, publish mqtt.Publish, op ...client.Option) (*client.Client, error) {
 	return client.Dial(mqtt.NewDial(cfg, subscribe, publish), op...)
+}
+
+func RedialMQTT(cfg *mqtt.Config, subscribe mqtt.Subscribe, publish mqtt.Publish, op ...client.Option) *client.Client {
+	return client.Redial(mqtt.NewDial(cfg, subscribe, publish), op...)
 }
 
 func Rabbitmq(addr string, cfg *rabbitmq.Config, op ...client.Option) (*client.Client, error) {
 	return client.Dial(rabbitmq.NewDial(addr, cfg), op...)
 }
 
+func RedialRabbitmq(addr string, cfg *rabbitmq.Config, op ...client.Option) *client.Client {
+	return client.Redial(rabbitmq.NewDial(addr, cfg), op...)
+}
+
 func Memory(key string, op ...client.Option) (*client.Client, error) {
 	return client.Dial(memory.NewDial(key), op...)
+}
+
+func RedialMemory(key string, op ...client.Option) *client.Client {
+	return client.Redial(memory.NewDial(key), op...)
 }
