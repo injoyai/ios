e4005e02c802252ac4c8ebf0b999dd50f4a7b9ed
injoyai <1113655791@qq.com>
2024-09-25 17:33:08 +0800

    测试1万个客户端数据交互是否有内存泄漏

---
+++ module/more_tcp/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/injoyai/ios"
+	"github.com/injoyai/ios/client"
+	"github.com/injoyai/ios/client/dial"
+	"github.com/injoyai/ios/server"
+	"github.com/injoyai/ios/server/listen"
+	"github.com/injoyai/logs"
+	"time"
+)
+
+/*
+测试很多tcp连接的性能,
+是否有内存泄漏,是否CPU占用过高
+
+ 1. 1个服务端,1万个客户端,客户端写入频率1次/s,服务端每条数据都响应,CPU型号为(i7-7700)
+    内存占用725.8MB,CPU使用率(5.0%~17.6%)
+*/
+func main() {
+
+	go func() {
+		err := listen.RunTCP(20001, func(s *server.Server) {
+			s.SetClientOption(func(c *client.Client) {
+				c.Logger.Debug(false)
+				c.Event.OnDealMessage = func(c *client.Client, msg ios.Acker) {
+					c.Write(msg.Payload())
+				}
+			})
+			go s.Timer(time.Second*5, func(s *server.Server) {
+				logs.Debug("客户端连接数量:", s.GetClientLen())
+			})
+		})
+		panic(err)
+	}()
+
+	for i := 0; i < 10000; i++ {
+		go func() {
+			dial.RedialTCP("127.0.0.1:20001", func(c *client.Client) {
+				c.Logger.Debug(false)
+				c.GoTimerWriter(time.Second, func(w ios.MoreWriter) error {
+					return w.WriteAny(time.Now().String())
+				})
+			}).Run()
+		}()
+	}
+
+	select {}
+
+}
