c0facc9b234f1cd94f1634172040625aeb820592
injoyai <1113655791@qq.com>
2024-10-28 09:46:55 +0800

    修复tcp设置ReadFrom失败的bug,tcp写了无效的ReadAck

--- example/frame/main.go
+++ example/frame/main.go
@@ -4,7 +4,7 @@ import (
 	"github.com/injoyai/ios"
 	"github.com/injoyai/ios/client"
 	"github.com/injoyai/ios/client/dial"
-	"github.com/injoyai/ios/module/frame"
+	"github.com/injoyai/ios/client/frame"
 	"github.com/injoyai/ios/server"
 	"github.com/injoyai/ios/server/listen"
 	"time"
@@ -14,12 +14,12 @@ func main() {
 
 	go listen.RunTCP(10089, func(s *server.Server) {
 		s.SetClientOption(func(c *client.Client) {
-			c.Event.WithFrame(frame.Entity)
+			c.Event.WithFrame(frame.Default)
 		})
 	})
 
 	dial.RedialTCP(":10089", func(c *client.Client) {
-		c.Event.WithFrame(frame.Entity)
+		c.Event.WithFrame(frame.Default)
 		c.GoTimerWriter(time.Second*5, func(w ios.MoreWriter) error {
 			return w.WriteAny(time.Now().String())
 		})
---
+++ example/test_read_from/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/injoyai/ios"
+	"github.com/injoyai/ios/client"
+	"github.com/injoyai/ios/client/dial"
+	"github.com/injoyai/ios/client/frame"
+	"github.com/injoyai/ios/server"
+	"github.com/injoyai/ios/server/listen"
+	"github.com/injoyai/logs"
+	"io"
+	"time"
+)
+
+func main() {
+
+	go listen.RunTCP(8080, func(s *server.Server) {
+		s.SetClientOption(func(c *client.Client) {
+			c.Logger.Debug(false)
+			c.Event.WithFrame(frame.Default)
+			c.Event.OnDealMessage = func(c *client.Client, msg ios.Acker) {
+				c.Write(msg.Payload())
+			}
+		})
+	})
+
+	dial.RedialTCP("127.0.0.1:8080", func(c *client.Client) {
+		logs.Debug(c.Event.OnReadFrom)
+		c.Event.OnWriteWith = frame.Default.WriteWith
+		c.Event.OnReadFrom = func(r io.Reader) ([]byte, error) {
+			logs.Debug("ReadFrom")
+			return frame.Default.ReadFrom(r)
+		}
+		c.GoTimerWriter(time.Second, func(w ios.MoreWriter) error {
+			return w.WriteAny(time.Now())
+		})
+	}).Run()
+
+}
