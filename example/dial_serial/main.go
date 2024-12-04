68eb67a79ff262e050ddcd5937a1da3abc98e59f
injoyai <1113655791@qq.com>
2024-12-04 17:06:53 +0800

    增加串口示例

---
+++ example/dial_serial/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"github.com/injoyai/ios/client"
+	"github.com/injoyai/ios/client/dial"
+	"github.com/injoyai/ios/module/serial"
+	"github.com/injoyai/logs"
+	"os"
+	"time"
+)
+
+func main() {
+	c, err := dial.Serial(&serial.Config{
+		Address:  "COM2",
+		BaudRate: 9600,
+		DataBits: 8,
+		StopBits: 1,
+		Parity:   "N",
+		Timeout:  time.Second * 10,
+	}, func(c *client.Client) {
+		c.Event.OnDealErr = func(c *client.Client, err error) error {
+			if err != nil && err.Error() == "serial: timeout" {
+				return nil
+			}
+			return err
+		}
+	})
+	logs.PanicErr(err)
+
+	go func() {
+		buf := bufio.NewReader(os.Stdin)
+		for {
+			bs, _, _ := buf.ReadLine()
+			bs = append(bs, '\r', '\n')
+			c.Write(bs)
+		}
+	}()
+	c.Run()
+}
