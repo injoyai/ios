af92475818c56767ccdb0abf0a511573e9b34ad8
injoyai <1113655791@qq.com>
2025-08-08 11:53:28 +0800

    增加OnWrite事件,可以重试,加锁的操作

--- client/config.go
+++ client/config.go
@@ -2,9 +2,11 @@ package client
 
 import (
 	"context"
+	"github.com/injoyai/conv"
 	"github.com/injoyai/ios"
 	"github.com/injoyai/ios/module/common"
 	"io"
+	"sync"
 	"time"
 )
 
@@ -19,7 +21,8 @@ type Event struct {
 	OnDisconnect  func(c *Client, err error)                                                                   //断开连接事件
 	OnReadFrom    func(r io.Reader) ([]byte, error)                                                            //读取数据事件,当类型是io.Reader才会触发
 	OnDealMessage func(c *Client, msg ios.Acker)                                                               //处理消息事件
-	OnWriteWith   func(bs []byte) ([]byte, error)                                                              //写入消息事件
+	OnWriteWith   func(bs []byte) ([]byte, error)                                                              //写入消息数据事件,例如封装数据格式
+	OnWrite       func(f func() error) error                                                                   //写入消息事件,例如并发安全,错误重试
 	OnKeyChange   func(c *Client, oldKey string)                                                               //修改标识事件
 	OnDealErr     func(c *Client, err error) error                                                             //修改错误信息事件,例翻译成中文
 }
@@ -40,8 +43,32 @@ type Info struct {
 	WriteBytes int       //本次连接,写入数据字节
 }
 
-// ReconnectWithInterval 按一定时间间隔进行重连
-func ReconnectWithInterval(t time.Duration) func(ctx context.Context, c *Client, dial ios.DialFunc) (ios.ReadWriteCloser, string, error) {
+// NewWriteSafe 写入并发安全,例如websocket不能并发写入
+func NewWriteSafe() func(f func() error) error {
+	mu := sync.Mutex{}
+	return func(f func() error) error {
+		mu.Lock()
+		defer mu.Unlock()
+		return f()
+	}
+}
+
+// NewWriteRetry 写入错误重试
+func NewWriteRetry(retry int, interval ...time.Duration) func(f func() error) error {
+	after := conv.Default(0, interval...)
+	return func(f func() error) (err error) {
+		for i := 0; i <= retry; i++ {
+			if err = f(); err == nil {
+				break
+			}
+			<-time.After(after)
+		}
+		return
+	}
+}
+
+// NewReconnectInterval 按一定时间间隔进行重连
+func NewReconnectInterval(t time.Duration) func(ctx context.Context, c *Client, dial ios.DialFunc) (ios.ReadWriteCloser, string, error) {
 	return func(ctx context.Context, c *Client, dial ios.DialFunc) (ios.ReadWriteCloser, string, error) {
 		r, k, err := dial(ctx)
 		if err == nil {
@@ -67,11 +94,11 @@ func ReconnectWithInterval(t time.Duration) func(ctx context.Context, c *Client,
 
 var (
 	// defaultReconnect 默认重连机制
-	defaultReconnect = ReconnectWithRetreat(time.Second*2, time.Second*32, 2)
+	defaultReconnect = NewReconnectRetreat(time.Second*2, time.Second*32, 2)
 )
 
-// ReconnectWithRetreat 退避重试
-func ReconnectWithRetreat(start, max time.Duration, multi uint8) func(ctx context.Context, c *Client, dial ios.DialFunc) (ios.ReadWriteCloser, string, error) {
+// NewReconnectRetreat 退避重试
+func NewReconnectRetreat(start, max time.Duration, multi uint8) func(ctx context.Context, c *Client, dial ios.DialFunc) (ios.ReadWriteCloser, string, error) {
 	if start < 0 {
 		start = time.Second * 2
 	}
@@ -109,15 +136,15 @@ func ReconnectWithRetreat(start, max time.Duration, multi uint8) func(ctx contex
 	}
 }
 
-// DealMessageWithChan 把数据写入到chan中
-func DealMessageWithChan(ch chan ios.Acker) func(c *Client, msg ios.Acker) {
+// NewDealMessageWithChan 把数据写入到chan中
+func NewDealMessageWithChan(ch chan ios.Acker) func(c *Client, msg ios.Acker) {
 	return func(c *Client, msg ios.Acker) {
 		ch <- msg
 	}
 }
 
-// DealMessageWithWriter 把数据写入到io.Writer中
-func DealMessageWithWriter(w io.Writer) func(c *Client, msg ios.Acker) {
+// NewDealMessageWithWriter 把数据写入到io.Writer中
+func NewDealMessageWithWriter(w io.Writer) func(c *Client, msg ios.Acker) {
 	return func(c *Client, msg ios.Acker) {
 		if _, err := w.Write(msg.Payload()); err == nil {
 			msg.Ack()
@@ -125,8 +152,8 @@ func DealMessageWithWriter(w io.Writer) func(c *Client, msg ios.Acker) {
 	}
 }
 
-// DisconnectWithAfter 断开连接等待
-func DisconnectWithAfter(t time.Duration) func(c *Client, err error) error {
+// NewDisconnectAfter 断开连接等待
+func NewDisconnectAfter(t time.Duration) func(c *Client, err error) error {
 	return func(c *Client, err error) error {
 		<-time.After(t)
 		return nil
